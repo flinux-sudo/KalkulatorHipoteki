ef5761d8d336375e8c2dde9a32894d98018d6e22
flinux-sudo <filipgondek78@gmail.com>
2020-09-21 23:28:57 +0200

    i added comments

--- src/com/kalkulator/Inputmortagecalculator.java
+++ src/com/kalkulator/Inputmortagecalculator.java
@@ -8,28 +8,29 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Inputmortagecalculator {
-    static int decimal=100;
-    static int months=12;
+    static int decimal=100;//zmienna na potrzebe zamiany na licabe dziesietną
+    static int months=12;// zmienna na potrzebe zamiany roku na miesiace
     public Scanner scaner= new Scanner(System.in);
+    static int positive=0;//tu chciałem zmienna ktora mi pomoze w while inty i float brac poduawge tylko wieksze od zera
 
 
     public float inputRate() {
         System.out.println("Rate (for year): ");
-        while (!scaner.hasNextFloat()) scaner.next();
-        float rate= scaner.nextFloat();
-        rate= rate / decimal / months;
+        while (!scaner.hasNextFloat()&&scaner.nextFloat()<(float)positive) scaner.next();//petla aby tylko skaner przyjmowal float  i wieksza niz zero(ale to cos nie działą)
+        float rate= scaner.nextFloat();//poviera nastepnego floata
+        rate= rate / decimal / months;//zamienia na liczbe dzisietna i dzieli na miesiac
 
         return rate;
     }
     public List <Integer> inputPricipalAndPeriod() {
         System.out.println("Principal: ");
-        while (!scaner.hasNextInt()) scaner.next();
+        while (!scaner.hasNextInt()&&scaner.nextInt()<positive) scaner.next();//tak jak w przypadku z float-em
         int  principal = scaner.nextInt();
         System.out.println("Period(in years): ");
-        while (!scaner.hasNextInt()) scaner.next();
+        while (!scaner.hasNextInt()&&scaner.nextInt()<positive) scaner.next();
         int period = scaner.nextInt();
-        period=period *12;
-        List <Integer> lista = new ArrayList<Integer>(1);
+        period=period *months;
+        List <Integer> lista = new ArrayList<Integer>(1);//lista abym mogł zwrócic dwie zmienne z metody
         lista.add(0,principal);
         lista.add(1,period);
         return lista;
--- src/com/kalkulator/Mortagecalculator.java
+++ src/com/kalkulator/Mortagecalculator.java
@@ -4,21 +4,21 @@ import java.text.DecimalFormat;
 import java.util.List;
 
 public class Mortagecalculator {
-
-    Inputmortagecalculator input=new Inputmortagecalculator();
+//    głowna klasa ktora pobiera dane od klasy ktora jest odpowiedzialna za input
+    Inputmortagecalculator input=new Inputmortagecalculator();//nowa instancja obiektu klasy input mam dostep do inputu
     public void calculateMortage(){
-        List<Integer> list=input.inputPricipalAndPeriod();
-        int principal= list.get(0);
+        List<Integer> list=input.inputPricipalAndPeriod();//lokalna lista dla listy (Integer) z klasu input
+        int principal= list.get(0);//przypisuje te zmienne ktore uzyskałem z klasy input do loaklnych
         float rate=input.inputRate();
         int period=list.get(1);
 
         float power= (float) Math.pow(1+rate,period);
         float multi= rate * power;
         float second=power-1;
-        float mortage = principal * (multi / second);
+        float mortage = principal * (multi / second);//obliczam hipoteke na bazie uzyskanych danych
         DecimalFormat df = new DecimalFormat("#.#");
-        df.setRoundingMode(RoundingMode.CEILING);
-        System.out.println("Mortage: "+df.format(mortage));
+        df.setRoundingMode(RoundingMode.CEILING);//zaokrąglam wynik ps tu chciałem dac do dwóch miejsc po przecinku ale nie mogłem znalesc wiec coz...
+        System.out.println("Mortage: "+df.format(mortage));//zaokrągla do częsci dziesitnych wiec działa
 
 
     }
