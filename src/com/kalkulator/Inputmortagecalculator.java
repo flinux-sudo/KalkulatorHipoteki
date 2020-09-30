474425d2bbedaf5edd933f812b4337880c1e69f5
flinux-sudo <filipgondek78@gmail.com>
2020-09-30 20:15:58 +0200

    i repaired wrong statement

--- src/com/kalkulator/Inputmortagecalculator.java
+++ src/com/kalkulator/Inputmortagecalculator.java
@@ -16,7 +16,8 @@ public class Inputmortagecalculator {
 
     public float inputRate() {
         System.out.println("Rate (for year): ");
-        while (!scaner.hasNextFloat()&&scaner.nextFloat()<(float)positive) scaner.next();//petla aby tylko skaner przyjmowal float  i wieksza niz zero(ale to cos nie działą)
+        while (!(scaner.hasNextFloat()) && scaner.nextFloat()>positive)
+            scaner.next();//petla aby tylko skaner przyjmowal float  i wieksza niz zero(ale to cos nie działą)
         float rate= scaner.nextFloat();//poviera nastepnego floata
         rate= rate / decimal / months;//zamienia na liczbe dzisietna i dzieli na miesiac
 
@@ -24,10 +25,10 @@ public class Inputmortagecalculator {
     }
     public List <Integer> inputPricipalAndPeriod() {
         System.out.println("Principal: ");
-        while (!scaner.hasNextInt()&&scaner.nextInt()<positive) scaner.next();//tak jak w przypadku z float-em
+        while (!(scaner.hasNextInt()) && scaner.nextInt()>positive) scaner.next();//tak jak w przypadku z float-em
         int  principal = scaner.nextInt();
         System.out.println("Period(in years): ");
-        while (!scaner.hasNextInt()&&scaner.nextInt()<positive) scaner.next();
+        while (!(scaner.hasNextInt()) && scaner.nextInt()>positive) scaner.next();
         int period = scaner.nextInt();
         period=period *months;
         List <Integer> lista = new ArrayList<Integer>(1);//lista abym mogł zwrócic dwie zmienne z metody
